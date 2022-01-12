8d10c49e80b327c43fdae607496a1e75c08eafc3
Ethan <than.banhlevu@doctoranywhere.com>
2022-01-12 22:58:37 +0700

    update some UI

--- app/src/main/java/com/ethan/bridj/utils/Constans.kt
+++ app/src/main/java/com/ethan/bridj/utils/Constans.kt
@@ -9,5 +9,5 @@ const val BASE_URL = "https://s3-ap-southeast-2.amazonaws.com/"
 object DateFormat {
     val DATE_LONG_FORMAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH)
     val STANDARD_DATETIME_FORMAT: DateTimeFormatter =
-        DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH)
+        DateTimeFormatter.ofPattern("yyyy MMM dd", Locale.ENGLISH)
 }
